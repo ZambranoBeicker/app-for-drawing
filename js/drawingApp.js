bed305ebf57c2e101aeb862e596bf45cdb65db3c
Beicker <beicker@gmail.com>
2019-12-26 21:07:39 -0500

    my 3 eventListeners added

--- js/drawingApp.js
+++ js/drawingApp.js
@@ -3,3 +3,26 @@ const ctx = canvas.getContext('2d')
 
 canvas.width = window.innerWidth
 canvas.height = window.innerheight
+
+// need to add into the ctx an addEventListener that will have a draw function
+
+/* need to create that draw funciton
+   it need to be:
+   
+    - create a line when mouse is pressed down
+    - when the button is up it must to stop
+    - the lines have to go toward the pointer coordenates*/
+
+
+//------------ All this could change ----------------------------------------------------
+ctx.addEventListener('mousedown', () =>{
+    startDraw()
+})
+
+ctx.addEventListener('mousemove', () =>{
+    Draw()
+})
+
+ctx.addEventListener('mouseup', () =>{
+    // Stop drawing
+})
